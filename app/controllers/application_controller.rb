a0b02890ebb496d78e3889ebf0cab5873b499cee
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-05 15:55:07 +0300

    updated get solutions with sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
+  set default_content_type: "application/json"
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+  ##GET BAKERIES/ID ROUTE
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+  ## GET BAKED_GOODS/BY PRICE
+  get'/baked_goods/by_price' do
+  sorted_goods = BakedGood.order(price: :desc)
+  sorted_goods.to_json
+  end
+  ##GET BAKED GOODS/MOST EXPENSIVE
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.order(price: :desc).first
+    most_expensive.to_json
+  end
 
 end
